e2d85f0e10127884e9384af00edc7aa1ef6b5588
Emil <emil.taciyev@gmail.com>
2021-05-01 01:56:55 +0400

    Perfected.

--- timesScript.js
+++ timesScript.js
@@ -19,25 +19,32 @@ if($("#searchInput").val()){
 $("#searchBtn").on("click", function(e){
     e.preventDefault()
     searchInput = $("#searchInput").val();
-    searchSelect = $("#searchSelect").val();
+    searchSelect = $("#mainSelect").val();
     searchSYear = $("#searchSYear").val();
     searchEYear = $("#searchEYear").val();
 
-    if(searchSYear && searchEYear){
-    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=" + searchInput + "&begin_date=" + searchSYear + "0101" + "&end_date=" + searchEYear + "0101";
-    console.log("yes")
+    if(searchSYear || searchEYear){
+        if(searchSYear && !searchEYear){
+            queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=" + searchInput + "&begin_date=" + searchSYear + "0101" 
+        }else if(searchEYear && !searchSYear){
+            queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=" + searchInput + "&end_date=" + searchEYear + "0101";
+        }else{
+        queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=" + searchInput + "&begin_date=" + searchSYear + "0101" + "&end_date=" + searchEYear + "0101";
+        }
     }else{
         queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=" + searchInput
-        console.log("no")
     }
-
+    console.log(searchSelect)
     $.ajax({
         url: queryURL,
         method: "GET"
       }).done(function(NYTData) {
+          if(NYTData.response.docs.length === 0){
+            $("#searchResult").prepend(`<div class="well text-center">This item does not exist</div>`)
+          }
           for(var i = 0; i < parseInt(searchSelect); i++ ){
             console.log(NYTData.response.docs[i].headline)
-            $("#searchResult").prepend(`<div class="well"><a target="_blank" href="${NYTData.response.docs[i].web_url}">${NYTData.response.docs[i].headline.main}</a></div>`)
+            $("#searchResult").prepend(`<div class="well"><a target="_blank" href="${NYTData.response.docs[i].web_url}"><strong style="font-size: 20px">${NYTData.response.docs[i].headline.main}</strong></a><p>${NYTData.response.docs[i].lead_paragraph}</p></div>`)
           }
       })
     
